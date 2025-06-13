12e8bf98c8c19a72c5d3439d4a0765f7a13db753
Vatsal-Panjjar <panjiarvatsal17@gmail.com>
2025-06-13 18:15:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,45 +3,45 @@ provider "azurerm" {
 subscription_id = "47131d78-dc13-4bdc-954f-375b6a9e79cf"
 } 
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg-vm-example"
+resource "azurerm_resource_group" "Puru" {
+  name     = "Puru"
   location = "Central India"     
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet-example"
+resource "azurerm_virtual_network" "vn-puru" {
+  name                = "vn-puru"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.Puru.location
+  resource_group_name = azurerm_resource_group.Puru.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-example"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "sub-1" {
+  name                 = "sub-1"
+  resource_group_name  = azurerm_resource_group.Puru.name
+  virtual_network_name = azurerm_virtual_network.Puru.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "nic-example"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = azurerm_resource_group.Puru.location
+  resource_group_name = azurerm_resource_group.Puru.name
 
   ip_configuration {
     name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.example.id   
+    subnet_id                     = azurerm_subnet.sub-1.id   
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "win-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_windows_virtual_machine" "Puru" {
+  name                = "Puru"
+  resource_group_name = azurerm_resource_group.Puru.name
+  location            = azurerm_resource_group.Puru.location
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  admin_password      = "P@ssw0rd1234!"
-  network_interface_ids = [azurerm_network_interface.example.id]
+  admin_username      = "Puru"
+  admin_password      = "rupupuru"
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
